b06acd2c0fb7c8690d9c4c598e434ddaab690d55
Dalibor Ristic <dalibor.ristic@rt-rk.com>
2019-12-20 11:03:28 +0100

    Express routing

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 // set constant in express
 app.set('port', 3000);
+
+app.get('/', function(req, res) {
+	console.log('GET the homepage');
+	res
+		.status(200)
+		.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', function(req, res) {
+	console.log('GET the json');
+	res
+		.status(200)
+		.json( {"jsonData" : true} );
+});
+
+app.get('/file', function(req, res) {
+	console.log('GET the file');
+	res
+		.status(200)
+		.sendFile(path.join(__dirname, 'app.js') );
+});
 
 const server = app.listen(app.get('port'), function() {
 	const port = server.address().port;
