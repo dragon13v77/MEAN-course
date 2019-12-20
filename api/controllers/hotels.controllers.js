c0009df2aa35f37679c39c3b13ba90323c5be8bb
Dalibor Ristic <dalibor.ristic@rt-rk.com>
2019-12-20 13:36:09 +0100

    Express - dynamic routes with URL parameters

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -6,3 +6,12 @@ module.exports.hotelsGetAll = function(req, res) {
 		.status(200)
 		.json( hotelData );
 };
+
+module.exports.hotelsGetOne = function(req, res) {
+	const hotelId = req.params.hotelId;
+	const thisHotel = hotelData[hotelId];
+	console.log('GET hotelId', hotelId);
+	res
+		.status(200)
+		.json( thisHotel );
+};
