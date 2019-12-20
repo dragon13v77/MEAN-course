585d417e876259269873c0e646fc3f106d682970
Dalibor Ristic <dalibor.ristic@rt-rk.com>
2019-12-20 10:47:13 +0100

    Adding express

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello Node');
+const express = require('express');
+const app = express();
+
+// set constant in express
+app.set('port', 3000);
+
+const server = app.listen(app.get('port'), function() {
+	const port = server.address().port;
+	console.log('Listening port ' + port);
+});
