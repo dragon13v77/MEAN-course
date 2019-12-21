e9c432c1bb8c5ae0f42b38470dede5a7f9e53b86
Dalibor Ristic <dalibor.ristic@rt-rk.com>
2019-12-21 13:59:53 +0100

    MongoDB - creating documents from Node

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -40,9 +40,32 @@ module.exports.hotelsGetOne = function(req, res) {
 };
 
 module.exports.hotelsAddOne = function(req, res) {
+	// database connection - permanent
+	const db = dbconn.get();
+	const collection = db.collection('hotels');
+	var newHotel;
+
+
 	console.log('Hotels add one');
-	console.log(req.body);
-	res
-		.status(200)
-		.json(req.body);
+
+	if (req.body && req.body.name && req.body.stars) {
+		console.log(req.body);
+
+		newHotel  = req.body;
+		newHotel.stars = parseInt(newHotel.stars, 10);
+
+		collection
+			.insertOne(newHotel, function(err, response) {
+				console.log(response);
+				res
+					.status(201)
+					.json(response.ops);
+			})
+	}
+	else {
+		console.log("Data missing from the body");
+		res
+			.status(400)
+			.json({message : "Required data missing from body"});
+	}
 };
