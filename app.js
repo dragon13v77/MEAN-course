bd16aa8b873c5c8984e99d4b2c2a1035cb858593
Dalibor Ristic <dalibor.ristic@rt-rk.com>
2019-12-22 16:37:21 +0100

    Building the SPA - Part 1

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(function(req, res, next) {
 // set static resources path
 // define subset of routs with first argument `/public`
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
 //app.get('/', function(req, res) {
 //	console.log('GET the homepage');
 //	res
